a2c1c7c4a48649af3ef6942b09c297743657b3b2
Jonghwa <whdghk3471@gmail.com>
2025-02-08 16:34:58 +0900

    recreate contentful

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -15,7 +15,7 @@ const config: GatsbyConfig = {
     {
       resolve: `gatsby-source-contentful`,
       options: {
-        spaceId: `flirxh618mai`,
+        spaceId: `pyyfjg2zynxl`,
         // Learn about environment variables: https://gatsby.dev/env-vars
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
       },
